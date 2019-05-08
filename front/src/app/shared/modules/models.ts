60fc2e22deb4c044b3bb7f5d19c8d33e59cfc5b3
Derbes <uderbes@gmail.com>
2019-05-08 22:46:25 +0600

    front

---
+++ front/src/app/shared/modules/models.ts
@@ -0,0 +1,42 @@
+import {Time} from '@angular/common';
+
+export interface IReview {
+  id: number;
+  review: string;
+  rating: number;
+  author: string;
+  submissionDate: Date;
+}
+
+export interface IFlight {
+  id: number;
+  companyName: string;
+  sourceLocation: string;
+  destinationLocation: string;
+  departureDate: Date;
+  departureTime: Time;
+  fareEconomy: any;
+  fareBusiness: any;
+  fareFirst: any;
+  numSeatsRemainingEconomy: number;
+  numSeatsRemainingBusiness: number;
+  numSeatsRemainingFirst: number;
+}
+
+export interface Hotel {
+  id: number;
+  dailyCost: any;
+  address: string;
+  city: string;
+  companyName: string;
+}
+
+export interface HotelList {
+  id: number;
+  name: string;
+}
+
+export interface FlightList {
+  id: number;
+  name: string;
+}
