f103f02d686721c5e359adf8e8ba30f400c21b20
Derbes <uderbes@gmail.com>
2019-05-14 02:47:37 +0600

    front-models-header-footer

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './src/app/components/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HotelListComponent } from './components/hotel-list/hotel-list.component';
+import { HotelComponent } from './components/hotel/hotel.component';
+import { FlightComponent } from './components/flight/flight.component';
+import { FlightListComponent } from './components/flight-list/flight-list.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HotelListComponent,
+    HotelComponent,
+    FlightComponent,
+    FlightListComponent,
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
