ddb9a8d0124a688b28280253c39f033918da3c34
dorieq <karasaevd@gmail.com>
2019-05-14 06:50:22 +0600

    front

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -9,14 +9,12 @@ import { HotelComponent } from './components/hotel/hotel.component';
 import { FlightComponent } from './components/flight/flight.component';
 import { FlightListComponent } from './components/flight-list/flight-list.component';
 import { LoginComponent } from './components/login/login.component';
-import { FooterComponent } from './components/footer/footer.component';
-<<<<<<< HEAD
-import { MainComponent } from './main/main.component';
-=======
+import { FooterComponent } from './components/footer/footer.component'
+import { MainComponent } from './components/main/main.component';
 import {HttpClientModule} from '@angular/common/http';
 import {MainService} from './shared/services/main.service';
 import {ProviderService} from './shared/services/provider.service';
->>>>>>> 30d14f1345d89c29182aa486d590c4c1d62d2c7f
+import { BasketComponent } from './components/basket/basket.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +26,8 @@ import {ProviderService} from './shared/services/provider.service';
     FlightListComponent,
     LoginComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    BasketComponent
   ],
   imports: [
     HttpClientModule,
